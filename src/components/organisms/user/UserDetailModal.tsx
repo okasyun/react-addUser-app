8ac55c4910fc1fc1013f1a2b0e4eedd27aa0d017
Shun Okamoto <44208228+okasyun@users.noreply.github.com>
2022-08-17 01:30:06 +0900

    Initial commit

---
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -0,0 +1,107 @@
+import { memo, VFC, useState, useEffect, ChangeEvent } from "react";
+import {
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalContent,
+  ModalCloseButton,
+  ModalOverlay,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  ModalFooter
+} from "@chakra-ui/react";
+
+import { User } from "../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
+
+type Props = {
+  user: User | null;
+  isOpen: boolean;
+  isAdmin?: boolean;
+  onClose: () => void;
+};
+
+export const UserDetailModal: VFC<Props> = memo((props) => {
+  const { user, isOpen, isAdmin = false, onClose } = props;
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {
+    setUsername(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUsername(e.target.value);
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+
+  const onChangePhone = (e: ChangeEvent<HTMLInputElement>) =>
+    setPhone(e.target.value);
+
+  const onClickUpdate = () => alert("更新しました");
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      autoFocus={false}
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent pb={2}>
+        <ModalHeader>ユーザー詳細</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody mx={4}>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel>名前</FormLabel>
+              <Input
+                value={username}
+                isReadOnly={!isAdmin}
+                onChange={onChangeUserName}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>フルネーム</FormLabel>
+              <Input
+                value={name}
+                isReadOnly={!isAdmin}
+                onChange={onChangeName}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>メール</FormLabel>
+              <Input
+                value={email}
+                isReadOnly={!isAdmin}
+                onChange={onChangeEmail}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>電話番号</FormLabel>
+              <Input
+                value={phone}
+                isReadOnly={!isAdmin}
+                onChange={onChangePhone}
+              />
+            </FormControl>
+          </Stack>
+        </ModalBody>
+        {isAdmin && (
+          <ModalFooter>
+            <PrimaryButton onClick={onClickUpdate}>更新</PrimaryButton>
+          </ModalFooter>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+});
---
+++ src/providers/LoginUserProvider.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { createContext, Dispatch, ReactNode, SetStateAction } from "react";
+
+import { User } from "../types/api/user";
+
+type LoginUser = User & { isAdmin: boolean };
+
+export type LoginUserContextType = {
+  loginUser: LoginUser | null;
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>;
+};
+
+export const LoginUserContext = createContext<LoginUserContextType>(
+  {} as LoginUserContextType
+);
+
+export const LoginUserProvider = (props: { children: ReactNode }) => {
+  const { children } = props;
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
+  return (
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      {children}
+    </LoginUserContext.Provider>
+  );
+};
